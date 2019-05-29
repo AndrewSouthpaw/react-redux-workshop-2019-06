1f9978f902a83a07ac4339fb6a4122caf7cee994
Andrew Smith <andrewsmith@alumni.stanford.edu>
2019-05-28 20:53:56 -0700

    FP.

---
+++ 01a_functionalProgramming/lecture/src/App.end.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Template!</p>
+        </header>
+      </div>
+    )
+  }
+}
