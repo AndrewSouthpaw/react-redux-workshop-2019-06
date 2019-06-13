411f67004b2c8efb476abdb0009965fca3facc6b
Andrew Smith <andrewsmith@alumni.stanford.edu>
2019-06-13 15:30:17 -0500

    Add helpers.

--- 07_reactTesting/exercise/src/lib/helpers.js
+++ 07_reactTesting/exercise/src/lib/helpers.js
@@ -23,3 +23,24 @@ export const reflush = async (wrapper) => {
   await flushPromises()
   wrapper.update()
 }
+
+export const sel = curry((dataTestId, wrapper) => wrapper.find(`[data-test-id="${dataTestId}"]`))
+
+/**
+ * Workaround for broken `findWhere` behavior for mounted components when searching by text
+ * https://github.com/airbnb/enzyme/issues/1566
+ */
+const textContent = (node) => {
+  try {
+    // enzyme sometimes blows up on text()
+    return node.text()
+  } catch (_e) {
+    return ''
+  }
+}
+
+// useful for finding a node by its text
+export const findByText = (text, wrapper, options = {}) => {
+  const comparator = options.exact ? x => textContent(x) === text : x => new RegExp(text).test(textContent(x))
+  return wrapper.findWhere(comparator).filterWhere(x => x.html().startsWith('<')).last()
+}
